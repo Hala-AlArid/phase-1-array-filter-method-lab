34e49d3251ad77625d3b0abdb3a6fff6b7fa3713
Hala AlArid <halaalarid99@gmail.com>
2022-11-02 01:27:01 +0200

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(arr, str){
+    const names = arr.filter(name => name.toUpperCase().includes(str.toUpperCase()));
+return names;
+}
+
+function fuzzyMatch(arr, str){
+    const names = arr.filter(name => name.charAt(0).toUpperCase() === str.charAt(0).toUpperCase());
+
+    return names;
+}
+
+function matchName(arr, str){
+    const names = arr.filter(n => n.name === (str) )
+
+    return names;
+}
